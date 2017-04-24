e06078bce2c5b6c8a8461018c441e3ac939ead66
bpowley <brendon.powley@gmail.com>
2017-04-24 14:48:54 -0600

    federation ing table

--- fedr_ings.php
+++ fedr_ings.php
@@ -13,19 +13,18 @@
 <script type="text/javascript" src="./fedr_ings.js"></script>
 </head>
 <body>
-  <div style="padding-left: 8px;">
-    <h3 style="text-align: center">Federation Ingredients Page</h3>
-    <p>This page should display the ingredients across all the open sites</p>
-    <table id="ings">
+  <div>
+    <h3 style="text-align: center;">Federation Ingredients Page</h3>
+    <p style="text-align: center;"> This page should display from sites thtat are open</p>
+    <table id="ings" class="table" style="margin: 0 auto;">
       <tr>
-        <th>Team Name</th>
         <th>Ingredient Name</th>
         <th>Unit</th>
         <th>Price</th>
-        <th>Url</th>
+        <th>URL</th>
       </tr>
     </table>
-    <p>
+    <p  style="text-align: center;">
       Status of Sites AJAX Call: <span id="output1">Unresponsive</span>
     </p>
   </div>
